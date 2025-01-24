5941c619660993703558b2916bfca4a0046423dc
IsaPirola <belapirola@gmail.com>
2025-01-24 19:51:39 -0300

    fix cors methods

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: "GET,POST",
+    methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   })
 );
